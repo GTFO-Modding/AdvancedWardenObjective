2134232775928c9c05c605a8b0680fe471d93387
Flowaria <wnsghgj1234@naver.com>
2022-07-29 00:44:58 +0900

    peak developer moment

--- ExtendedWardenEvents/EntryPoint.cs
+++ ExtendedWardenEvents/EntryPoint.cs
@@ -6,6 +6,7 @@ using ExtendedWardenEvents.WEE;
 namespace ExtendedWardenEvents
 {
     [BepInPlugin("WardenEventExtension", "EventsExt", VersionInfo.Version)]
+    [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
     internal class EntryPoint : BasePlugin
     {
         public unsafe override void Load()
