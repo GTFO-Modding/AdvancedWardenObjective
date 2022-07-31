e5af28c9e9685dd1920b66c164ae4b2bc67605fa
Flowaria <wnsghgj1234@naver.com>
2022-07-31 21:57:29 +0900

    Trigger Door Alarm event bug fix

--- ExtendedWardenEvents/WEE/Events/SecDoor/TriggerSecurityDoorAlarmEvent.cs
+++ ExtendedWardenEvents/WEE/Events/SecDoor/TriggerSecurityDoorAlarmEvent.cs
@@ -21,8 +21,15 @@ namespace ExtendedWardenEvents.WEE.Events.SecDoor
                 return;
             }
 
-            door.UseChainedPuzzleOrUnlock(LocalPlayer.Owner);
-            LogDebug($"{zone.NavInfo.GetFormattedText(LG_NavInfoFormat.Full_And_Number_No_Formatting)} Alarm Triggered!");
+            if (door.m_locks.ChainedPuzzleToSolve != null)
+            {
+                door.m_sync.AttemptDoorInteraction(eDoorInteractionType.ActivateChainedPuzzle, 0.0f, 0.0f, default, null);
+                LogDebug($"{zone.NavInfo.GetFormattedText(LG_NavInfoFormat.Full_And_Number_No_Formatting)} Alarm Triggered!");
+            }
+            else
+            {
+                LogDebug($"{zone.NavInfo.GetFormattedText(LG_NavInfoFormat.Full_And_Number_No_Formatting)} Does no have chainedpuzzle to activate");
+            }
         }
     }
 }
